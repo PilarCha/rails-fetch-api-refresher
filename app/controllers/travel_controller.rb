a956f7a2da209e9a8bb96e35c2c3d92b443a0a08
Pilar Chavez <pchavez92@gmail.com>
2022-02-25 20:19:29 -0800

    finished up the api call private method

--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -10,4 +10,18 @@ class TravelController < ApplicationController
       return render action :index
     end
   end
+
+  private
+  def request_api(url)
+    response = Excon.get(
+      url      
+    )
+
+    return nill if response.status != 200
+
+    JSON.parse(response.body)
+  end
+
+  def find_country(name)
+    request_api("https://restcountries.com/v3.1/name/#{URI.encode(name)}")
 end
